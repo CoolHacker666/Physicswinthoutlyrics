76ec2b2ddddca8d77347a9f50c557bd202e91faf
CoolHacker666 <79114824104mix@gmail.com>
2016-05-19 18:10:02 +0200

    Add files via upload

---
+++ StatisticActivity.java
@@ -0,0 +1,14 @@
+package ru.mixa201.physics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatisticActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+        setTitle("Статистика");
+    }
+}
